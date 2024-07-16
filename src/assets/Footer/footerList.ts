fa4e1584876f4bcf1c1837c08094889b9a2a5628
Aliiusuf <ialisuf@mail.ru>
2024-07-16 21:53:22 +0300

    commit

--- src/assets/Footer/footerList.ts
+++ src/assets/Footer/footerList.ts
@@ -1,12 +1,12 @@
-import { GrServices } from "react-icons/gr";
 import { GoHome } from "react-icons/go";
 import { LuMessageCircle } from "react-icons/lu";
 import { HiOutlineVideoCamera } from "react-icons/hi";
 import { FaPhotoVideo } from "react-icons/fa";
+import { PiSquaresFourLight } from "react-icons/pi";
 export const footerList = [
   { title: "Главная", icon: GoHome, link: "/" },
-  { title: "Сервисы", icon: GrServices, link: "/services" },
+  { title: "Сервисы", icon: PiSquaresFourLight, link: "/services" },
   { title: "Чаты", icon: LuMessageCircle, link: "/im" },
-  { title: "Клипы", icon: HiOutlineVideoCamera, link: "/video" },
-  { title: "Видео", icon: FaPhotoVideo, link: "/services" },
+  { title: "Клипы", icon: FaPhotoVideo, link: "/video" },
+  { title: "Видео", icon: HiOutlineVideoCamera, link: "/video" },
 ];
